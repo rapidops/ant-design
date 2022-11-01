fd7fdf9dd3a91f4116f55d936544d6899b3e54e9
Ravi113 <ravi.mehta@rapidops.com>
2022-11-01 16:35:34 +0530

    Published by Ant Design a11080f1ec9f3ef71499bdd09d1c4040914ecdd5 (HEAD -> refs/heads/master, refs/remotes/origin/master, refs/remotes/origin/HEAD) Merge pull request #72 from rapidops/merchandising-design

---
+++ es/icon/index.js
@@ -0,0 +1,6 @@
+import devWarning from '../_util/devWarning';
+var Icon = function Icon() {
+  devWarning(false, 'Icon', 'Empty Icon');
+  return null;
+};
+export default Icon;
---
+++ lib/version/version.d.ts
@@ -0,0 +1,2 @@
+declare const _default: "7.0.65";
+export default _default;
